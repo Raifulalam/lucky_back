0b7775e4e8a4aed28fd211abb88658c4f136bfbd
Raifulalam <raifulalam0123@gmail.com>
2024-12-22 09:04:24 +0545

    add more products

--- productsData.js
+++ productsData.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const products = [
+    // Refrigerators
     {
         image: "image/doubledoor/9D5A8F698D4550F-RT28A32216U.png",
         name: "RT28A32216R Samsung Double Door Refrigerator with Digital Inverter Technology, 253L",
@@ -17,64 +18,6 @@ const products = [
         category: "Refrigerators"
     },
     {
-        image: "image/doubledoor/1ECB5DF2089DD3E-RT28A32216R.png",
-        name: "RT28A32216R Samsung Double Door Refrigerator with Digital Inverter Technology, 253L",
-        link: "details.html",
-        details: "Refrigerator of 100l samsung 22452",
-        rating: {
-            stars: 4.5,
-            count: 87
-        },
-        mrp: 60990,
-        price: 51000,
-        keywords: [
-            "doubledoor",
-            "rt28a32216r",
-            "253",
-            "refrigerator",
-            "samsung",
-        ],
-        category: "Refrigerators"
-    },
-    {
-        image: "image/doubledoor/9D5A8F698D4550F-RT28A32216U.png",
-        name: "RT28A32216U Samsung Double Door Refrigerator with Digital Inverter Technology, 253L",
-        rating: {
-            stars: 4,
-            count: 127
-        },
-        mrp: 60990,
-        price: 51000,
-        keywords: [
-            "doubledoor",
-            "rt28a32216u",
-            "253",
-            "refrigerator",
-            "samsung",
-        ],
-        category: "Refrigerators"
-    },
-    {
-        id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-        image: "image/doubledoor/11D12E847973D5A-RT28C3221CR.png",
-        name: "RT28C3221CR Samsung Double Door Refrigerator, 253L",
-        rating: {
-            stars: 4.5,
-            count: 56
-        },
-        mrp: 61990,
-        price: 51500,
-        keywords: [
-            "doubledoor",
-            "rt28a3221cr",
-            "253",
-            "refrigerator",
-            "samsung",
-        ],
-        category: "Refrigerators"
-    },
-    {
-        id: "54e0eccd-8f36-462b-b68a-8182611d9add",
         image: "image/doubledoor/11DEAB55E1C3117-RT37C4521S8.png",
         name: "RT37C4521S8 Samsung Double Door Refrigerator, 345L",
         rating: {
@@ -84,167 +27,286 @@ const products = [
         mrp: 84990,
         price: 71000,
         keywords: [
-            "doubledoor",
-            "rt37c4521s8",
-            "345",
-            "refrigerator",
-            "samsung",
+            "doubledoor", "rt37c4521s8", "345", "refrigerator", "samsung",
         ],
         category: "Refrigerators"
     },
     {
-        id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
-        image: "image/doubledoor/A99F06C790B076B-RT28A3022GS.png",
-        name: "RT28A3022GS Samsung Double Door Refrigerator with Digital Inverter Technology, 253L",
+        image: "image/doubledoor/LG-GLT255.png",
+        name: "LG GLT255 Double Door Refrigerator, 255L",
         rating: {
-            stars: 4,
-            count: 37
+            stars: 4.6,
+            count: 300
         },
-        mrp: 56490,
-        price: 46500,
+        mrp: 59990,
+        price: 51000,
         keywords: [
-            "doubledoor",
-            "rt28a3022gs",
-            "253",
-            "refrigerator",
-            "samsung",
+            "doubledoor", "lg", "255", "refrigerator",
         ],
         category: "Refrigerators"
     },
     {
-        id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
-        image: "image/doubledoor/AF918959F338CE2-RT28C3221CU.png",
-        name: "RT28C3221CU Samsung Double Door Refrigerator, 253L",
+        image: "image/doubledoor/CG-DoubleDoor-265L.png",
+        name: "CG 265L Double Door Refrigerator",
         rating: {
-            stars: 4.5,
-            count: 175
+            stars: 4.4,
+            count: 155
         },
-        mrp: 61990,
-        price: 51500,
+        mrp: 50990,
+        price: 43000,
         keywords: [
-            "doubledoor",
-            "rt28c3221cu",
-            "253",
-            "refrigerator",
-            "samsung",
+            "doubledoor", "cg", "265", "refrigerator",
         ],
         category: "Refrigerators"
     },
     {
-        id: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
-        image: "image/doubledoor/F889302D3166DF5-RT30K3342S8.png",
-        name: "RT30K3342S8 Samsung Double Door Refrigerator, 275L",
+        image: "image/doubledoor/Whirlpool-WD255.png",
+        name: "Whirlpool 255L Double Door Refrigerator",
         rating: {
-            stars: 4.5,
-            count: 317
+            stars: 4.7,
+            count: 250
         },
-        mrp: 67990,
-        price: 55500,
+        mrp: 63990,
+        price: 53000,
         keywords: [
-            "doubledoor",
-            "rt30k3342s8",
-            "275",
-            "refrigerator",
-            "samsung",
+            "doubledoor", "whirlpool", "255", "refrigerator",
         ],
         category: "Refrigerators"
     },
-    // Single door refrigerators
     {
-        id: "77919bbe-0e56-475b-adde-4f24dfed3a04",
-        image: "image/Single-door/1DA2BEA520BC02C-RR20C20C2RH.png",
-        name: "RR20C20C2RH Samsung Single Door Refrigerator, 192L",
+        image: "image/doubledoor/Haier-HR265.png",
+        name: "Haier 265L Double Door Refrigerator",
+        rating: {
+            stars: 4.6,
+            count: 210
+        },
+        mrp: 57990,
+        price: 49000,
+        keywords: [
+            "doubledoor", "haier", "265", "refrigerator",
+        ],
+        category: "Refrigerators"
+    },
+
+    // Washing Machines
+    {
+        id: "89bf2e77-02d4-4be3-8484-b897ad0bcf9a",
+        image: "image/washingmachine/LG-T8.png",
+        name: "LG T8 Top Load Washing Machine, 8KG",
         rating: {
             stars: 4.5,
-            count: 144
+            count: 200
         },
-        mrp: 32990,
-        price: 27500,
+        mrp: 45000,
+        price: 38000,
         keywords: [
-            "singledoor",
-            "192",
-            "refrigerator",
-            "rr20c20c2rh",
-            "samsung",
+            "topload", "lg", "8kg", "washingmachine", "digitalinverter",
         ],
-        category: "Refrigerators"
+        category: "WashingMachines"
     },
     {
-        id: "3fdfe8d6-9a15-4979-b459-585b0d0545b9",
-        image: "image/Single-door/2A2CA29862D7AB6-RR20T282ZR8.png",
-        name: "RR20T282ZR8 Samsung Single Door Refrigerator with Digital Inverter Compressor, 192L",
+        id: "bd59c562-4a69-4f7f-bfdd-b28bb957e7cc",
+        image: "image/washingmachine/Whirlpool-Fuzzy.png",
+        name: "Whirlpool 7.5KG Fuzzy Logic Washing Machine",
+        rating: {
+            stars: 4.3,
+            count: 170
+        },
+        mrp: 38000,
+        price: 31000,
+        keywords: [
+            "topload", "whirlpool", "7.5kg", "washingmachine", "fuzzylogic",
+        ],
+        category: "WashingMachines"
+    },
+    {
+        id: "89c34e27-f41f-4696-8fbd-9c03e2746d2f",
+        image: "image/washingmachine/Haier-HW105.png",
+        name: "Haier 10KG Top Load Washing Machine",
+        rating: {
+            stars: 4.6,
+            count: 250
+        },
+        mrp: 55000,
+        price: 45000,
+        keywords: [
+            "topload", "haier", "10kg", "washingmachine",
+        ],
+        category: "WashingMachines"
+    },
+    {
+        id: "36c7084d-3b60-4700-a5b0-8fd5a1d35e9a",
+        image: "image/washingmachine/Samsung-WM10.png",
+        name: "Samsung 10KG Front Load Washing Machine",
+        rating: {
+            stars: 4.7,
+            count: 180
+        },
+        mrp: 65000,
+        price: 53000,
+        keywords: [
+            "frontload", "samsung", "10kg", "washingmachine",
+        ],
+        category: "WashingMachines"
+    },
+
+    // Air Conditioners
+    {
+        id: "1a44b2c4-ff4f-492e-b7fc-63f0d91467fc",
+        image: "image/airconditioner/Samsung-AR18.png",
+        name: "Samsung AR18 Air Conditioner, 1.5 Ton",
+        rating: {
+            stars: 4.7,
+            count: 120
+        },
+        mrp: 55000,
+        price: 46000,
+        keywords: [
+            "airconditioner", "samsung", "1.5ton", "ac", "digitalinverter",
+        ],
+        category: "AirConditioners"
+    },
+    {
+        id: "4c7fd3c6-93ff-4b77-bf98-2f47c8c3f2b7",
+        image: "image/airconditioner/LG-LS-Q18CNZA.png",
+        name: "LG LS-Q18CNZA Air Conditioner, 1.5 Ton",
+        rating: {
+            stars: 4.6,
+            count: 98
+        },
+        mrp: 53000,
+        price: 45000,
+        keywords: [
+            "airconditioner", "lg", "1.5ton", "ac",
+        ],
+        category: "AirConditioners"
+    },
+    {
+        id: "6f998f7a-fc5b-4722-94c1-7ed4b02f66da",
+        image: "image/airconditioner/Whirlpool-AC18.png",
+        name: "Whirlpool 1.5 Ton Air Conditioner",
+        rating: {
+            stars: 4.6,
+            count: 150
+        },
+        mrp: 54000,
+        price: 46000,
+        keywords: [
+            "airconditioner", "whirlpool", "1.5ton", "ac",
+        ],
+        category: "AirConditioners"
+    },
+
+    // Home Appliances
+    {
+        id: "c5e3b810-f7e5-4748-a990-f8f31c5e91ed",
+        image: "image/homeappliance/Whirlpool-Microwave.png",
+        name: "Whirlpool 20L Microwave Oven",
+        rating: {
+            stars: 4.4,
+            count: 75
+        },
+        mrp: 15000,
+        price: 12000,
+        keywords: [
+            "microwave", "whirlpool", "oven", "homeappliance",
+        ],
+        category: "HomeAppliances"
+    },
+    {
+        id: "3b0a249d-8965-45b9-bb51-ff9dfffbf9d4",
+        image: "image/homeappliance/LG-Blender.png",
+        name: "LG 600W Blender",
+        rating: {
+            stars: 4.2,
+            count: 60
+        },
+        mrp: 8000,
+        price: 6500,
+        keywords: [
+            "blender", "lg", "homeappliance",
+        ],
+        category: "HomeAppliances"
+    },
+
+    // Kitchen Appliances
+    {
+        id: "3498bf76-d55e-4378-b8d1-fdd35a6f7c0e",
+        image: "image/kitchenappliance/LG-Refrigerator.png",
+        name: "LG 270L Double Door Refrigerator",
+        rating: {
+            stars: 4.8,
+            count: 230
+        },
+        mrp: 51000,
+        price: 43000,
+        keywords: [
+            "doubledoor", "lg", "kitchenappliance", "refrigerator",
+        ],
+        category: "KitchenAppliances"
+    },
+    {
+        id: "b77c6ed5-1fd6-4207-b238-1b8b6993a732",
+        image: "image/kitchenappliance/Whirlpool-Dishwasher.png",
+        name: "Whirlpool Dishwasher 12 Place Settings",
         rating: {
             stars: 4.5,
-            count: 305
+            count: 45
         },
-        mrp: 41990,
-        price: 36000,
+        mrp: 25000,
+        price: 21000,
         keywords: [
-            "singledoor",
-            "192",
-            "refrigerator",
-            "rr20t282zr8",
-            "samsung",
+            "dishwasher", "whirlpool", "kitchenappliance",
         ],
-        category: "Refrigerators"
+        category: "KitchenAppliances"
     },
+
+    // Music & Home Theater
     {
-        id: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
-        image: "image/Single-door/7F470F4B6BA5539-RR20C2Z226U.png",
-        name: "RR20C2Z226U Samsung Single Door Refrigerator, 192L",
-        rating: {
-            stars: 4,
-            count: 89
-        },
-        mrp: 39590,
-        price: 33500,
-        keywords: [
-            "singledoor",
-            "192",
-            "refrigerator",
-            "rr20c2z226u",
-            "samsung",
-        ],
-        category: "Refrigerators"
-    },
-    {
-        id: "5968897c-4d27-4872-89f6-5bcb052746d7",
-        image: "image/Single-door/8E7A9442734E2F4-RR20C2Z226R.png",
-        name: "RR20C2Z226R Samsung Single Door Refrigerator, 192L",
+        id: "ada7db24-cf3f-4de5-bf87-1cf2e520568a",
+        image: "image/musicht/sony-home-theater.png",
+        name: "Sony 5.1 Channel Home Theater System",
         rating: {
             stars: 4.5,
-            count: 235
+            count: 200
         },
-        mrp: 39590,
-        price: 33500,
+        mrp: 25000,
+        price: 22000,
         keywords: [
-            "singledoor",
-            "192",
-            "refrigerator",
-            "rr20c2z226r",
-            "samsung",
+            "hometheater", "sony", "sound", "music",
         ],
-        category: "Refrigerators"
+        category: "MusicHomeTheater"
     },
     {
-        id: "aad29d11-ea98-41ee-9285-b916638cac4a",
-        image: "image/Single-door/563F7C13FC6004D-RR20M282ZS8.png",
-        name: "RR20M282ZS8 Samsung Single Door Refrigerator with Digital Inverter Technology, 192L",
+        id: "6d9c04fa-d07f-4c87-b3ad-bc1a87bc2cd6",
+        image: "image/musicht/Samsung-Soundbar.png",
+        name: "Samsung 2.1 Channel Soundbar with Subwoofer",
         rating: {
-            stars: 4.5,
-            count: 30
+            stars: 4.6,
+            count: 180
         },
-        mrp: 38990,
-        price: 33000,
+        mrp: 18000,
+        price: 15000,
         keywords: [
-            "singledoor",
-            "192",
-            "refrigerator",
-            "rr20m282zs8",
-            "samsung",
+            "soundbar", "samsung", "hometheater", "music",
         ],
-        category: "Refrigerators"
-    }
+        category: "MusicHomeTheater"
+    },
+    {
+        id: "acf7f7d3-b8c5-43ac-88b4-540106b12b6f",
+        image: "image/musicht/LG-Soundbar.png",
+        name: "LG 3.1 Channel Soundbar with Wireless Subwoofer",
+        rating: {
+            stars: 4.3,
+            count: 120
+        },
+        mrp: 22000,
+        price: 19000,
+        keywords: [
+            "soundbar", "lg", "hometheater", "music",
+        ],
+        category: "MusicHomeTheater"
+    },
 ];
 
 module.exports = products;
