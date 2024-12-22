c496dff7ce0f785783a22ca25eedd3ce58d0c7fd
Raifulalam <raifulalam0123@gmail.com>
2024-12-22 11:43:50 +0545

    add more products

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const ProductCategory = require('./Models/ProductsCategoryModel');
 
 const cors = require('cors');
 
-const productData = require('./productsData');
-const productCategories = require('./productsCategories');
+// const productData = require('./productsData');
+// const productCategories = require('./productsCategories');
 
 
 
@@ -36,15 +36,15 @@ mongoose.connect(mongoURI)
 
 // Post the data to the database
 
-Product.insertMany(productData)
-    .then(() => console.log("Products inserted"))
-    .catch(err => console.log("Error inserting products:", err));
+// Product.insertMany(productData)
+//     .then(() => console.log("Products inserted"))
+//     .catch(err => console.log("Error inserting products:", err));
 
 
-// insert productCategory
-ProductCategory.insertMany(productCategories)
-    .then(() => console.log("Product Categories inserted"))
-    .catch(err => console.log("Error inserting product categories:", err));
+// // insert productCategory
+// ProductCategory.insertMany(productCategories)
+//     .then(() => console.log("Product Categories inserted"))
+//     .catch(err => console.log("Error inserting product categories:", err));
 
 
 
