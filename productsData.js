38b1ea5fc929aa640c053bf94c788565bc253d96
Raifulalam <raifulalam0123@gmail.com>
2024-12-24 19:17:51 +0545

    add products

--- productsData.js
+++ productsData.js
@@ -3,7 +3,7 @@ const products = [
 
     {
         category: "AirConditioners",
-        price: "98,310.00",
+        price: 98310.00, // Converted from "98,310.00"
         description: "",
         brand: "LG",
         name: "S4H18TZCAA",
@@ -11,11 +11,11 @@ const products = [
         capacity: "1.5 Ton",
         keywords: [],
         model: "S4H18TZCAA",
-        mrp: "1,26,090.00"
+        mrp: 126090.00 // Converted from "1,26,090.00"
     },
     {
         category: "AirConditioners",
-        price: "1,27,803.00",
+        price: 127803.00, // Converted from "1,27,803.00"
         description: "",
         brand: "LG",
         name: "S4H24TZCAA",
@@ -23,11 +23,11 @@ const products = [
         capacity: "2 Ton",
         keywords: [],
         model: "S4H24TZCAA",
-        mrp: "1,63,890.00"
+        mrp: 163890.00 // Converted from "1,63,890.00"
     },
     {
         category: "AirConditioners",
-        price: "55,781.00",
+        price: 55781.00, // Converted from "55,781.00"
         description: "",
         brand: "LG",
         name: "UT18.NQDRO",
@@ -35,11 +35,11 @@ const products = [
         capacity: "1.5 Ton",
         keywords: [],
         model: "UT18.NQDRO",
-        mrp: "71,590.00"
+        mrp: 71590.00 // Converted from "71,590.00"
     },
     {
         category: "AirConditioners",
-        price: "99,553.00",
+        price: 99553.00, // Converted from "99,553.00"
         description: "",
         brand: "LG",
         name: "UU18.UEDRO",
@@ -47,11 +47,11 @@ const products = [
         capacity: "1.5 Ton",
         keywords: [],
         model: "UU18.UEDRO",
-        mrp: "1,27,690.00"
+        mrp: 127690.00 // Converted from "1,27,690.00"
     },
     {
         category: "AirConditioners",
-        price: "22,770.00",
+        price: 22770.00, // Converted from "22,770.00"
         description: "",
         brand: "LG",
         name: "PTMCHWO",
@@ -59,11 +59,11 @@ const products = [
         capacity: "Above 1 Ton",
         keywords: [],
         model: "PTMCHWO",
-        mrp: "29,190.00"
+        mrp: 29190.00 // Converted from "29,190.00"
     },
     {
         category: "AirConditioners",
-        price: "1,78,104.00",
+        price: 178104.00, // Converted from "1,78,104.00"
         description: "",
         brand: "LG",
         name: "GRAND TOTAL",
@@ -71,11 +71,11 @@ const products = [
         capacity: "",
         keywords: [],
         model: "GRAND TOTAL",
-        mrp: "2,28,470.00"
+        mrp: 228470.00 // Converted from "2,28,470.00"
     },
     {
         category: "AirConditioners",
-        price: "79,439.00",
+        price: 79439.00, // Converted from "79,439.00"
         description: "",
         brand: "LG",
         name: "ATNW18GPLS1",
@@ -83,11 +83,11 @@ const products = [
         capacity: "1.5 Ton",
         keywords: [],
         model: "ATNW18GPLS1",
-        mrp: "1,01,890.00"
+        mrp: 101890.00 // Converted from "1,01,890.00"
     },
     {
         category: "AirConditioners",
-        price: "1,13,226.00",
+        price: 113226.00, // Converted from "1,13,226.00"
         description: "",
         brand: "LG",
         name: "ATUW18GPLS1",
@@ -95,11 +95,11 @@ const products = [
         capacity: "1.5 Ton",
         keywords: [],
         model: "ATUW18GPLS1",
-        mrp: "1,45,190.00"
+        mrp: 145190.00 // Converted from "1,45,190.00"
     },
     {
         category: "AirConditioners",
-        price: "22,770.00",
+        price: 22770.00, // Converted from "22,770.00"
         description: "",
         brand: "LG",
         name: "PTMCHWO",
@@ -107,11 +107,11 @@ const products = [
         capacity: "Above 1 Ton",
         keywords: [],
         model: "PTMCHWO",
-        mrp: "29,190.00"
+        mrp: 29190.00 // Converted from "29,190.00"
     },
     {
         category: "AirConditioners",
-        price: "2,15,435.00",
+        price: 215435.00, // Converted from "2,15,435.00"
         description: "",
         brand: "LG",
         name: "GRAND TOTAL",
@@ -119,11 +119,11 @@ const products = [
         capacity: "",
         keywords: [],
         model: "GRAND TOTAL",
-        mrp: "2,76,270.00"
+        mrp: 276270.00 // Converted from "2,76,270.00"
     },
     {
         category: "AirConditioners",
-        price: "78,705.00",
+        price: 78705.00, // Converted from "78,705.00"
         description: "",
         brand: "LG",
         name: "ATNW24GPLS1",
@@ -131,11 +131,11 @@ const products = [
         capacity: "2 Ton",
         keywords: [],
         model: "ATNW24GPLS1",
-        mrp: "1,00,990.00"
+        mrp: 100990.00 // Converted from "1,00,990.00"
     },
     {
         category: "AirConditioners",
-        price: "1,39,668.00",
+        price: 139668.00, // Converted from "1,39,668.00"
         description: "",
         brand: "LG",
         name: "ATUW24GPLS1",
@@ -143,11 +143,11 @@ const products = [
         capacity: "2 Ton",
         keywords: [],
         model: "ATUW24GPLS1",
-        mrp: "1,79,090.00"
+        mrp: 179090.00 // Converted from "1,79,090.00"
     },
     {
         category: "AirConditioners",
-        price: "22,770.00",
+        price: 22770.00, // Converted from "22,770.00"
         description: "",
         brand: "LG",
         name: "PTMCHWO",
@@ -155,7 +155,7 @@ const products = [
         capacity: "Above 1 Ton",
         keywords: [],
         model: "PTMCHWO",
-        mrp: "29,190.00"
+        mrp: 29190.00 // Converted from "29,190.00"
     }
 
 ];
